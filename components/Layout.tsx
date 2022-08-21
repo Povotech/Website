be92a15f2d45059287c8d0539c954ee5abe52a22
Mathieu KHALEM <mkhalem@directskills.com>
2022-08-21 18:30:10 +0200

    Add Footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,25 @@
 import { Container, Stack, StackProps } from "@chakra-ui/react";
 import { FC, PropsWithChildren } from "react";
+import { Footer } from "./Footer";
 import { NavBar } from "./NavBar";
 
 export const Layout: FC<PropsWithChildren<StackProps>> = ({
   children,
   ...props
 }) => (
-  <Container maxW={"5xl"}>
-    <NavBar />
-    <Stack
-      textAlign={"center"}
-      align={"center"}
-      spacing={{ base: 8, md: 10 }}
-      py={{ base: 10, md: 20 }}
-      {...props}
-    >
-      {children}
-    </Stack>
-  </Container>
+  <>
+    <Container minH={"100vh"} maxW={"5xl"}>
+      <NavBar />
+      <Stack
+        textAlign={"center"}
+        align={"center"}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 10, md: 20 }}
+        {...props}
+      >
+        {children}
+      </Stack>
+    </Container>
+    <Footer />
+  </>
 );
