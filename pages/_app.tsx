20aac834893923c2d8a0362950920c1e7a7c6e86
Mathieu KHALEM <mkhalem@directskills.com>
2022-08-21 17:48:55 +0200

    Add SEO to the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 const colors = {
   brand: {
@@ -14,6 +15,12 @@ const theme = extendTheme({ colors });
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+      <title>PovoTech</title>
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="porto vecchio, povo, tech, ordinateur, depannage, informatique" />
+        <meta name="author" content="PovoTech" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
